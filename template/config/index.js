77422114903a73877b6225ab22b8ee5a12ca5304
Mgen <m@mgenware.com>
2017-11-01 15:32:42 +0800

    Add dev build config

--- template/config/index.js
+++ template/config/index.js
@@ -7,8 +7,8 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../dist/prod/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist/prod/'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
@@ -37,5 +37,23 @@ module.exports = {
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false
-  }
+  },
+  devBuild: {
+    env: require('./dev.env'),
+    index: path.resolve(__dirname, '../dist/dev/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist/dev/'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: false,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: false,
+  },
 }
