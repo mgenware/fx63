47868cc513963d8fc229412e38e86fab8d80735c
Mgen <m@mgenware.com>
2017-11-01 12:19:02 +0800

    Turn "prefer-template" off

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
     }],
     {{/if_eq}}
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'prefer-template': 'off'
   }
 }
