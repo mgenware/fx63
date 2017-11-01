a434a410f3f65fbc6f7e006faa145c3862758225
Mgen <m@mgenware.com>
2017-11-02 01:35:30 +0800

    Fix dev config not resolved

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -18,7 +18,7 @@ module.exports = {
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.getDevConfig().assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
